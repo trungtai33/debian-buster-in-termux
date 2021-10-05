73d562e79e662fe9175c893b26010e919041e1dd
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-05 19:31:43 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
-if [ -d "$PREFIX/share/debian-buster" ]; then
-printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling Debian Buster, please wait...\n\e[31m"
-rm -rf "$PREFIX/share/debian-buster"
-rm -f "$PREFIX/bin/start-debian-buster"
+distro_name="Debian Buster"
+if [ -d "${PREFIX}/share/debian-buster" ]; then
+printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[31m"
+rm -rf "${PREFIX}/share/debian-buster"
+rm -f "${PREFIX}/bin/start-debian-buster"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
-printf "\n\e[31mError: distribution Debian Buster is not installed.\n\n\e[0m"
+printf "\n\e[31mError: distribution ${distro_name} is not installed.\n\n\e[0m"
