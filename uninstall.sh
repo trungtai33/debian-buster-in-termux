f9360efaab4af2f3ffdd180fde9d9943d31d4df5
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-06 07:08:07 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
+directory="debian-buster"
 distro_name="Debian Buster"
-if [ -d "${PREFIX}/share/debian-buster" ]; then
+if [ -d "${PREFIX}/share/${directory}" ]; then
 printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[31m"
-rm -rf "${PREFIX}/share/debian-buster"
-rm -f "${PREFIX}/bin/start-debian-buster"
+rm -rf "${PREFIX}/share/${directory}"
+rm -f "${PREFIX}/bin/start-${directory}"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
